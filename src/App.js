abd58980ba06d28697382d343e96ff369ee9e39e
jayantmanapure1 <jayantmanapure1@gmail.com>
2022-02-28 14:16:10 +0530

    change in app.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const[task,setTask]=useState('');
   const[tasks,setTasks]=useState([]);
+  const[task,setTask]=useState('');
+  
   function addTask(){
     setTasks(tasks=>[...tasks, task]);
     setTask('');
@@ -15,17 +16,17 @@ function App() {
       <div className='container'></div>
       <h1 className='display-6 text-center heading' >To Do List</h1>
       <div className='row '>
-        <div className='col-md-5   add'><h3>Add Task</h3>
-        <form>
+        <div className='col-md-5  ms-3 text-center'><h3>Add Task</h3>
+        <div>
           <input type="text" name="task" className='form-control mt-3' placeholder="Add Task"
           onChange={(e)=>{setTask(e.target.value)}} value={task}/>
           <button type="submit" className='btn btn-warning w-100 mt-5 mb-2' onClick={addTask}>Add Task</button>
-        </form>
+        </div>
           </div>
-        <div className='col-md-5  show '> 
+        <div className='col-md-5  show  text-center'> 
         <h3>Show Task</h3>
         <ol> {
-          tasks.map(task=>{
+          tasks.map((task)=>{
             return (
               <div className='alert alert-primary'>
                 <li key={task} className='ms-2'>{task}</li>
