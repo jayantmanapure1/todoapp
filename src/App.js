5c118ef7149ff6d076e7e6db73199283cd28ae94
jayantmanapure1 <jayantmanapure1@gmail.com>
2022-02-28 13:12:56 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const[task,setTask]=useState('');
+  const[tasks,setTasks]=useState([]);
+  function addTask(){
+    setTasks(tasks=>[...tasks, task]);
+    setTask('');
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+      <div className='container'></div>
+      <h1 className='display-6 text-center heading' >To Do List</h1>
+      <div className='row '>
+        <div className='col-md-5   add'><h3>Add Task</h3>
+        <form>
+          <input type="text" name="task" className='form-control mt-3' placeholder="Add Task"
+          onChange={(e)=>{setTask(e.target.value)}} value={task}/>
+          <button type="submit" className='btn btn-warning w-100 mt-5 mb-2' onClick={addTask}>Add Task</button>
+        </form>
+          </div>
+        <div className='col-md-5  show '> 
+        <h3>Show Task</h3>
+        <ol> {
+          tasks.map(task=>{
+            return (
+              <div className='alert alert-primary'>
+                <li key={task} className='ms-2'>{task}</li>
+              </div>
+            
+            );
+          })
+}
+</ol>
+       
+        </div>
+      </div>
     </div>
+    </>
+    
   );
 }
 
